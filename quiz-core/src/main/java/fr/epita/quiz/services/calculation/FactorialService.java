f2233c8edab9bc4f7f2f74318b126245f410dddf
Kakashi <Kakashi@192.168.228.129>
2019-04-27 08:36:57 +0200

    Sample

--- quiz-core/src/main/java/fr/epita/quiz/services/calculation/FactorialService.java
+++ quiz-core/src/main/java/fr/epita/quiz/services/calculation/FactorialService.java
@@ -5,10 +5,12 @@ public class FactorialService {
 	
 	public Integer calculate(Integer depth) {
 		if (depth <= 1 ) {
+			System.out.println("This is a sample");
 			return 1;
 		}else {
 			return calculate(depth - 1) * depth;
 		}
+		
 		
 	}
 
---
+++ quiz-core/src/main/java/sample.java
@@ -0,0 +1,8 @@
+
+public class sample {
+	private void Syso() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
