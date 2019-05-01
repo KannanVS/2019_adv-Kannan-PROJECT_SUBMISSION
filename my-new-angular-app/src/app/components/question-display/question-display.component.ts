0aeaf798f7d61c2fe3b4edbaa4538aaf97c2b49d
KannanVS <kannansanthanam97@gmail.com>
2019-05-01 20:43:40 +0200

    Updated

---
+++ my-new-angular-app/src/app/components/question-display/question-display.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './question-display.component.html',
+  styleUrls: ['./question-display.component.css']
+})
+export class AppComponent {
+  title = 'Display a Question';
+}
